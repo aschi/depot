ffeb631ad0366c0ff0a132243e69c1bb141e4b7f
Adrian Schmid <schmiad1@students.zhaw.ch>
2012-06-09 13:21:47 +0200

    Kapitel 10 & 11

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -6,14 +6,6 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-#---
-# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 require 'test_helper'
 
 class LineItemsControllerTest < ActionController::TestCase
@@ -34,32 +26,43 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, :product_id => products(:ruby).id
+      post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
-    get :show, :id => @line_item.to_param
+    get :show, id: @line_item
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => @line_item.to_param
+    get :edit, id: @line_item
     assert_response :success
   end
 
   test "should update line_item" do
-    put :update, :id => @line_item.to_param, :line_item => @line_item.attributes
+    put :update, id: @line_item, line_item: @line_item.attributes
     assert_redirected_to line_item_path(assigns(:line_item))
   end
 
   test "should destroy line_item" do
     assert_difference('LineItem.count', -1) do
-      delete :destroy, :id => @line_item.to_param
+      delete :destroy, id: @line_item
     end
 
     assert_redirected_to line_items_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end 
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
   end
 end
